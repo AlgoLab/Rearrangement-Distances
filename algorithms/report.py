80bb271cdae4e140bec9374d535b9711dd46279c
Simone Ciccolella <simone.ciccolella@unimib.it>
2022-04-14 10:57:05 +0200

    update for tex table

--- algorithms/report.py
+++ algorithms/report.py
@@ -1,12 +1,18 @@
 import sys
 import pandas as pd
+from natsort import natsorted
+
+TEX = True
 
 # Run with:
 # python3 report.py simulations/new/*.csv > report_sim.md
 
 if __name__ == '__main__':
     files = sys.argv[1:]
-    for f in files:
+    if TEX:
+        print("\\begin{tabular}{ l|l|r|r|r|r } ")
+        print('\t$n$ & $d$ & Max a.f. & Median a.f. & $Approx$ run time (s) & $Exac$ run time (s) \\\\\hline')
+    for f in natsorted(files):
         df = pd.read_csv(f)
         if len(df) == 0:
             continue
@@ -30,11 +36,18 @@ if __name__ == '__main__':
         aproxfact_std = df["approx_factor"].std()
         aproxfact_median = df["approx_factor"].median()
         aproxfact_max = df["approx_factor"].max()
+        
+        if TEX:
+            if tot == 100:
+                print(f'\t{n} & {d} & {aproxfact_max:.2f} & {aproxfact_median:.2f} & {approx_time:.2e} & {fpt_time:.2e} \\\\')
+        else:           
+            print(f'# N={n}, d={d}, k={k}, simulations={tot}, max approx factor = {aproxfact_max:.4f}')
+            print('| | mean | std | median | avg time (s) |')
+            print('|--|--|--|--|--|')
+            print(f'| approximation | {approx_mean:.4f} | {approx_std:.4f} | {approx_median:.4f} | {approx_time:.4E} |')
+            print(f'| FPT | {fpt_mean:.4f} | {fpt_std:.4f} | {fpt_median:.4f} | {fpt_time:.4E} |')
+            print(f'| approx factor | {aproxfact_mean:.4f} | {aproxfact_std:.4f} | {aproxfact_median:.4f} | |')
+            print()
 
-        print(f'# N={n}, d={d}, k={k}, simulations={tot}, max approx factor = {aproxfact_max:.4f}')
-        print('| | mean | std | median | avg time (s) |')
-        print('|--|--|--|--|--|')
-        print(f'| approximation | {approx_mean:.4f} | {approx_std:.4f} | {approx_median:.4f} | {approx_time:.4E} |')
-        print(f'| FPT | {fpt_mean:.4f} | {fpt_std:.4f} | {fpt_median:.4f} | {fpt_time:.4E} |')
-        print(f'| approx factor | {aproxfact_mean:.4f} | {aproxfact_std:.4f} | {aproxfact_median:.4f} | |')
-        print()
+    if TEX:
+        print("\end{tabular}")
